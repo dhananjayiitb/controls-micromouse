82c5a61907af4ed417e5772fe48fd4889efc39ed
Dhananjay <dhankejriwal@cse.iitb.ac.in>
2021-10-13 17:18:21 +0530

    Server

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 app.debug = True
 
 TIME_STEP = 0.000001
+MAX = 40
 ONE_SEC = int(1/TIME_STEP)
 OMEGA_ERROR = 20
 RADIUS = 2
@@ -17,7 +18,7 @@ right_set = 0
 
 @app.route('/')
 def run():
-    return str(left_set) + "," + str(right_set)
+    return str(int(255.0*left_set/MAX)) + "," + str(int(255.0*right_set/MAX))
 
 app.run(host="192.168.0.177")
 
@@ -25,9 +26,8 @@ x_set = 16
 theta_set = 0
 
 while(True):
-    print(str(left_set) + "," + str(right_set))
-    left_set = K_P_L*(x_set-x_current) + K_P_A*(theta_set-theta_current)
-    right_set = K_P_L*(x_set-x_current) - K_P_A*(theta_set-theta_current)
+    left_set = K_P_L*(x_set-x_current) + K_P_A*(theta_set-theta_current) if K_P_L*(x_set-x_current) + K_P_A*(theta_set-theta_current) < MAX else MAX
+    right_set = K_P_L*(x_set-x_current) - K_P_A*(theta_set-theta_current) if K_P_L*(x_set-x_current) - K_P_A*(theta_set-theta_current) < MAX else MAX
     v_set = 1.0*RADIUS*(left_set + right_set)/2
     omega_set = 180.0*RADIUS*(left_set - right_set)/CIRCUMFERENCE
     x_current = (x_current+v_set*TIME_STEP)
