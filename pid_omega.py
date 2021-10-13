203337835f4d61f95b7d769f8036a62a40a4923b
Dhananjay <dhankejriwal@cse.iitb.ac.in>
2021-10-13 15:35:21 +0530

    Updates

--- pid_omega.py
+++ pid_omega.py
@@ -4,11 +4,13 @@ TIME_STEP = 0.000001
 ONE_SEC = int(1/TIME_STEP)
 OMEGA_ERROR = 20
 RADIUS = 2
-CIRCUMFERENCE = 30
-print("K_P_Linear: ",end="")
-K_P_L = float(input())
-print("K_P_Angular: ",end="")
-K_P_A = float(input())
+CIRCUMFERENCE = 32
+# print("K_P_Linear: ",end="")
+# K_P_L = float(input())
+# print("K_P_Angular: ",end="")
+# K_P_A = float(input())
+K_P_L = 10
+K_P_A = 1.75
 
 x_set = [0] # X to be reached
 x_current = [0] # X of bot
@@ -17,23 +19,23 @@ theta_current = [0] # Angle of bot
 left_set = [0] # Left Omega
 right_set = [0] # Right Omega
 
-for i in range(1*ONE_SEC): # Still 
+# for i in range(1*ONE_SEC): # Still 
+#     left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
+#     right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
+#     v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
+#     omega_set = 1.0*RADIUS*(left_set[-1]-right_set[-1])/2
+#     x_set.append(x_set[-1])
+#     x_current.append(x_current[-1]+v_set*TIME_STEP)
+#     theta_set.append(theta_set[-1])
+#     theta_current.append(theta_current[-1]+omega_set*TIME_STEP)
+
+x_set.append(16)
+
+for i in range(int(0.25*ONE_SEC)): 
     left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
     right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
     v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
-    omega_set = 1.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
-    x_set.append(x_set[-1])
-    x_current.append(x_current[-1]+v_set*TIME_STEP)
-    theta_set.append(theta_set[-1])
-    theta_current.append(theta_current[-1]+omega_set*TIME_STEP)
-
-x_set.append(10)
-
-for i in range(int(0.8*ONE_SEC)): #Forward 
-    left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
-    right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
-    v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
-    omega_set = 1.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
+    omega_set = 180.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
     x_set.append(x_set[-1])
     x_current.append(x_current[-1]+v_set*TIME_STEP)
     theta_set.append(theta_set[-1])
@@ -41,11 +43,48 @@ for i in range(int(0.8*ONE_SEC)): #Forward
 
 theta_set.append(90)
 
-for i in range(1*ONE_SEC): # Turn 
+for i in range(int(0.125*ONE_SEC)): 
     left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
     right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
     v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
-    omega_set = 1.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
+    omega_set = 180.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
+    x_set.append(x_set[-1])
+    x_current.append(x_current[-1]+v_set*TIME_STEP)
+    theta_set.append(theta_set[-1])
+    theta_current.append(theta_current[-1]+omega_set*TIME_STEP)
+
+x_set.append(32)
+
+for i in range(int(0.5*ONE_SEC)): 
+    left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
+    right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
+    v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
+    omega_set = 180.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
+    x_set.append(x_set[-1])
+    x_current.append(x_current[-1]+v_set*TIME_STEP)
+    theta_set.append(theta_set[-1])
+    theta_current.append(theta_current[-1]+omega_set*TIME_STEP)
+
+theta_set.append(0)
+
+for i in range(int(0.125*ONE_SEC)): 
+    left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
+    right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
+    v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
+    omega_set = 180.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
+    x_set.append(x_set[-1])
+    x_current.append(x_current[-1]+v_set*TIME_STEP)
+    theta_set.append(theta_set[-1])
+    theta_current.append(theta_current[-1]+omega_set*TIME_STEP)
+
+
+theta_set.append(-90)
+
+for i in range(int(0.125*ONE_SEC)): 
+    left_set.append(K_P_L*(x_set[-1]-x_current[-1]) + K_P_A*(theta_set[-1]-theta_current[-1]))
+    right_set.append(K_P_L*(x_set[-1]-x_current[-1]) - K_P_A*(theta_set[-1]-theta_current[-1]))
+    v_set = 1.0*RADIUS*(left_set[-1] + right_set[-1])/2
+    omega_set = 180.0*RADIUS*(left_set[-1]-right_set[-1])/CIRCUMFERENCE
     x_set.append(x_set[-1])
     x_current.append(x_current[-1]+v_set*TIME_STEP)
     theta_set.append(theta_set[-1])
